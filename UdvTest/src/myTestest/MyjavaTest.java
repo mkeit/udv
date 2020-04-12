082b8146ae9ac7c6f5cfce2753c3292403a4fb9d
mkeit@yahoo.com <mkeit@yahoo.com>
2020-04-12 19:06:12 +0200

    Test commit message

---
+++ UdvTest/src/myTestest/MyjavaTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package myTestest;
+
+/**
+ * @author mamadou
+ *
+ */
+import java.time.LocalDateTime; 
+public class MyjavaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDateTime myObj = LocalDateTime.now();
+	      System.out.println("Det er min javakode: " + myObj);
+	}
+
+}
